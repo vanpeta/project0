ba1c44ec92a9509d2dd1a36b08b1a15b5980f4ad
Carlos Allende <carlosallendesanz@gmail.com>
2016-03-14 11:11:28 -0700

    simplified

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 
 var turn=1
 var done = "no"
-var a1 = document.getElementById("a1").textContent
-var a2 = document.getElementById("a2").textContent
-var a3 = document.getElementById("a3").textContent
-var b1 = document.getElementById("b1").textContent
-var b2 = document.getElementById("b2").textContent
-var b3 = document.getElementById("b3").textContent
-var c1 = document.getElementById("c1").textContent
-var c2 = document.getElementById("c2").textContent
-var c3 = document.getElementById("c3").textContent
+var a1 = ""
+var a2 = ""
+var a3 = ""
+var b1 = ""
+var b2 = ""
+var b3 = ""
+var c1 = ""
+var c2 = ""
+var c3 = ""
 function stop (){
-    done = "yes"
+  done = "yes"
 }
 function displayText(){
   if (turn>1 && turn%2==0){
